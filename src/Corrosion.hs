45222ab1787026f96c4dbcb3763f4eb32bf98be2
Markus Läll <markus.l2ll@gmail.com>
2023-12-18 20:24:44 +0200

    Move cli helpers to InternalPrelude

--- src/Corrosion.hs
+++ src/Corrosion.hs
@@ -11,21 +11,11 @@ import Filesystem as Export
 import Builtins as Export
 import Coreutils as Export
 
-import Control.Monad as Export (forM, forM_, when)
-import Control.Exception as Export (throwIO)
+import InternalPrelude
 
 -- * Helpers
 
 import Data.Time qualified as Time
-import Options.Applicative qualified as O
-
--- * CLI
-
-simpleCli :: String -> O.Parser a -> IO a
-simpleCli header cli = O.execParser $ O.info (O.helper <*> cli) $ O.fullDesc <> O.header header
-
-headerDescrCli :: String -> String -> O.Parser a -> IO a
-headerDescrCli header descr cli = O.execParser $ O.info (O.helper <*> cli) $ O.fullDesc <> O.header header <> O.progDesc descr
 
 -- * Measure time
 
@@ -49,19 +39,3 @@ timePrint_ action = timePrint "_" action
 
 todo :: a
 todo = undefined
-
--- * Generic
-
-labelPrint :: (MonadIO m, Show a) => String -> a -> m ()
-labelPrint label a = liftIO $ putStrLn $ label <> " " <> show a
-
--- * (,) is (:=)
-
-type (:=) a b = (a, b)
-
-pattern (:=) :: a -> b -> a := b
-pattern (:=) a b = (a, b)
-
-infixr 0 :=
-
-{-# COMPLETE (:=) #-}
